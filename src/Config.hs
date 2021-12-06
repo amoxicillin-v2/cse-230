4e1358f8fa34ec59e78e380d0c2f07908b440285
yeyangzhou98 <yez002@ucsd.edu>
2021-12-05 16:19:48 -0800

    finish 1st version

--- src/Config.hs
+++ src/Config.hs
@@ -1,4 +1,30 @@
-module GameLevel where
+module Config where
 
--- TODO:
--- func load() -> List[board]
+import Block
+import Printer
+
+
+rawBlocks :: [[Char]]
+rawBlocks = [
+  ['E', 'G', 'W', 'M'],
+  ['E', 'G', 'W', 'M'],
+  ['E', 'G', 'W', 'M'],
+  ['E', 'G', 'W', 'M']]
+
+rawNums :: [[Int]]
+rawNums = [
+  [0, 0, 0, 1],
+  [0, 0, 0, 1],
+  [0, 0, 0, 1],
+  [0, 0, 0, 1]]
+
+loadBoard :: [[Char]] -> [[Int]] -> [[Block]]
+loadBoard [] [] = []
+loadBoard (blockline:bls) (numline:nls) = (genRow blockline numline):(loadBoard bls nls)
+  where
+    genRow [] [] = []
+    genRow (block:bs) (num:ns) =
+      if block=='E' then EmptyBlock {}: (genRow bs ns)
+      else if block=='G' then GoalBlock {}: (genRow bs ns)
+      else if block=='W' then WallBlock {}: (genRow bs ns)
+      else MonsterBlock {attack=num}: (genRow bs ns)
