e488c2c6acde50cec83d772806377723806dd6e2
Zhiyuan Xiang <z1xiang@ucsd.edu>
2021-12-05 16:33:52 -0800

    finish printer with brick

--- src/Printer.hs
+++ src/Printer.hs
@@ -3,6 +3,10 @@ module Printer where
 import System.IO
 import Block
 import Character (Character (NewCharacter), health, stepCount, fov, xPos, yPos)
+import Brick
+import Brick.Widgets.Border (borderWithLabel, vBorder, hBorder)
+import Brick.Widgets.Border.Style (unicode)
+import Brick.Widgets.Center (center, hCenter, centerWith)
 
 -- TODO: printer and brick lib
 
@@ -61,10 +65,10 @@ printLevel board character = do
   --horizontalSeperator
 
 
---printPrompt str = do
---  hSetBuffering stdin NoBuffering
---  hSetBuffering stdout NoBuffering
---  putStr str
+printPrompt str = do
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
+  putStr str
 
 --horizontalSeperator = do
 --  putStrLn "---- ---- ---- ---- ----"
@@ -74,7 +78,7 @@ testCharacter:: Character
 testCharacter = NewCharacter{
     health = 6,
     stepCount = 0,
-    fov = 2,
+    fov = 1,
     yPos = 0,
     xPos = 0
 }
@@ -86,17 +90,103 @@ testBoard = [[WallBlock, EmptyBlock, WallBlock], [EmptyBlock, EmptyBlock, WallBl
 -- // ---- CHARACTER ----
 -- // HP: 6
 -- // MOV: 0
--- // FOV: 2
+-- // FOV: 1
 -- // ---- --------- ----
 -- <BLANKLINE>
 -- <BLANKLINE>
 --  ---- ---- ----
--- | X  |    | X  |
+-- | X  |    | ?  |
 --  ---- ---- ----
--- |    |    | X  |
+-- |    |    | ?  |
 --  ---- ---- ----
--- |    | X  |    |
+-- | ?  | ?  | ?  |
 --  ---- ---- ----
 -- <BLANKLINE>
 --
 
+data GameStatus = MkGameStatus {
+    board:: [[Block]],
+    character:: Character
+}
+
+drawCharacter :: Character -> Widget String
+drawCharacter cha = do
+  let h = show(health cha)
+  let count = show(stepCount cha)
+  let f = show(fov cha)
+  vBox [
+    str "---- CHARACTER ----",
+    str ("HP: " ++ h),
+    str ("MOV: " ++ count),
+    str ("FOV: " ++ f),
+    str "-------------------"]
+
+drawBlock :: Block -> Bool -> Widget String
+drawBlock _ True = centerWith (Just ' ') blockN
+drawBlock EmptyBlock False = center blockE
+drawBlock WallBlock False = centerWith (Just 'X') blockW
+drawBlock (MonsterBlock _) False = center blockM
+drawBlock GoalBlock False = center blockG
+drawBlock (TeatureBlock _) False = center blockT
+
+drawLine :: [Block] -> Int -> Int -> Int -> [Widget String]
+drawLine [] _ _ _ = []
+drawLine (b : remain) left right current = do
+  if (current >= left && current <= right) then (drawBlock b False) : (drawLine remain left right (current + 1))
+  else (drawBlock b True) : (drawLine remain left right (current + 1))
+
+drawMaze :: [[Block]] -> Int -> Int -> Int -> Int -> Int -> [Widget String]
+drawMaze [] _ _ _ _ _ = []
+drawMaze (bs : remain)left right up down current = do
+  if (current >= up && current <= down) then (hLine (drawLine bs left right 0)) : (drawMaze remain left right up down (current + 1))
+  else (hLine (drawLine bs 0 0 1)) : (drawMaze remain left right up down (current + 1))
+
+hLine :: [Widget n] -> Widget n
+hLine (b:bs) = hBox (b : [vBorder <+> b | b <- bs])
+hLine _      = emptyWidget
+
+vLine :: [Widget n] -> Widget n
+vLine (b:bs) = vBox (b : [hBorder <=> b | b <- bs])
+vLine _      = emptyWidget
+
+drawGame :: GameStatus -> [Widget String]
+drawGame gs = do
+  let cha = character gs
+  let b = board gs
+  let left = (xPos cha) - (fov cha)
+  let right = (xPos cha) + (fov cha)
+  let up = (yPos cha) - (fov cha)
+  let down = (yPos cha) + (fov cha)
+  [vBox [
+    hCenter (drawCharacter cha),
+    center (vLine (drawMaze b left right up down 0))]]
+
+blockM, blockT, blockG, blockN, blockE, blockW, blockC:: Widget n
+blockM = vBox [
+  str " X X ",
+  str "  A  ",
+  str " VWV "]
+blockG = vBox [
+  str "||>  ",
+  str "||> >",
+  str "||   "]
+blockT = vBox [
+  str "  *  ",
+  str " *** ",
+  str "  *  "]
+blockW = vBox [
+  str "XXXXX",
+  str "XXXXX",
+  str "XXXXX"]
+blockN = vBox [
+  str "?????",
+  str "?????",
+  str "?????"]
+blockE = vBox [
+  str "     ",
+  str "     ",
+  str "     "]
+blockC = vBox [
+  str "  *  ",
+  str "\\| | ",
+  str "  =  "]
